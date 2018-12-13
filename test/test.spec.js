7c376eaf9a7587fe64815c3a566b050d8d691a7d
frumania.com <info@frumania.com>
2018-12-13 21:32:59 +0100

    init

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,10 +1,20 @@
+var j_username = element(by.id('j_username'));
+var j_password = element(by.id('j_password'));
+var logOnFormSubmit = element(by.id('logOnFormSubmit'));
+
 var meAreaHeaderButton = element(by.id('meAreaHeaderButton'));
 var signOut = element(by.id('logoutBtn'));
 var okBtn = element(by.id('__mbox-btn-0'));
 
+const config = browser.testrunner.config;
+
 describe('test', function() {
 
   it('Start FLP', function() {
+
+    j_username.sendKeys("info@frumania.com");
+    j_password.sendKeys("J#6i/{%lY+].");
+    logOnFormSubmit.click();
 
     expect(meAreaHeaderButton.isPresent()).toBeTruthy();
     meAreaHeaderButton.click();
