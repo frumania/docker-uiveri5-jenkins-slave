2992495a79845f534556945ffa06be612b98f8b0
frumania.com <info@frumania.com>
2018-12-14 10:24:20 +0100

    update

--- test/test2.sh
+++ test/test2.sh
@@ -1,1 +1,1 @@
-visualtest --v --config '{"auth": {"sapcloud-form": {"user": "$user","pass": "$password"}}}'
+exec visualtest --v --config '{"auth": {"sapcloud-form": {"user": "$user","pass": "$password"}}}'
