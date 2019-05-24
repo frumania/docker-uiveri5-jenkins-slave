2ab96031a81a18bd9956d93a1bf7df91de9612ee
Marcel Törpe <info@frumania.com>
2019-05-24 20:29:40 +0200

    check

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,4 +1,5 @@
 var meAreaHeaderButton = element(by.id('meAreaHeaderButton'));
+var popOver = element(by.id('sapUshellMeAreaPopover'));
 var signOut = element(by.id('__list0-4-logoutBtn'));
 var okBtn = element(by.id('__mbox-btn-0'));
 
@@ -10,10 +11,12 @@ describe('test', function() {
 
     expect(meAreaHeaderButton.isPresent()).toBeTruthy();
     meAreaHeaderButton.click();
+    expect(popOver.isPresent()).toBeTruthy();	  
+	  
     signOut.click();
     
     console.log("Logging Out...");
-		okBtn.click();
+    okBtn.click();
     browser.sleep(2000);
     
   });
