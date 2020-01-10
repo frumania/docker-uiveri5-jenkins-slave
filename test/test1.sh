f964e9b141f61631cdd9c2febc043d8be7120f35
Marcel Törpe <info@frumania.com>
2020-01-10 20:18:49 +0100

    Update test1.sh

--- test/test1.sh
+++ test/test1.sh
@@ -1,2 +1,2 @@
-exec uiveri5 --v --seleniumAddress http://localhost:4444/wd/hub --config "{\"auth\": {\"sapcloud-form\": {\"user\": \"$user\",\"pass\": \"$password\"}}}"
+uiveri5 --v --seleniumAddress http://localhost:4444/wd/hub --config "{\"auth\": {\"sapcloud-form\": {\"user\": \"$user\",\"pass\": \"$password\"}}}"
 echo "hello world"
