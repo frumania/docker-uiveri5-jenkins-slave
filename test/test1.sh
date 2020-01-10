1457d00d08327bbc663be12aa686783816601bdb
Marcel Törpe <info@frumania.com>
2020-01-10 20:26:58 +0100

    Update test1.sh

--- test/test1.sh
+++ test/test1.sh
@@ -1,2 +1,4 @@
 uiveri5 --v --seleniumAddress http://localhost:4444/wd/hub --config "{\"auth\": {\"sapcloud-form\": {\"user\": \"$user\",\"pass\": \"$password\"}}}"
+echo "exit1"
 exit
+echo "exit2"
