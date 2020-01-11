068837f1af23874d118c8f5c41013279fb42e6a8
Marcel Törpe <info@frumania.com>
2020-01-11 16:25:05 +0100

    Update test1.sh

--- test/test1.sh
+++ test/test1.sh
@@ -1,3 +1,4 @@
-uiveri5 --v --seleniumAddress http://localhost:4444/wd/hub --config "{\"auth\": {\"sapcloud-form\": {\"user\": \"$user\",\"pass\": \"$password\"}}}"
+#uiveri5 --v --seleniumAddress http://localhost:4444/wd/hub --config "{\"auth\": {\"sapcloud-form\": {\"user\": \"$user\",\"pass\": \"$password\"}}}"
+/usr/bin/docker ps
 docker ps
 echo "hello"
