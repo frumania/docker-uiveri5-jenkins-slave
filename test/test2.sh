eb95b6a464a6322351616d2c85907e9e524da227
Marcel Törpe <info@frumania.com>
2020-01-11 18:41:55 +0100

    Update test2.sh

--- test/test2.sh
+++ test/test2.sh
@@ -1,2 +1,3 @@
 #exec uiveri5 --v --useSeleniumJar true --config.connectionConfigs.direct.binaries.chromedriver.version=75.0.3770.90 --config "{\"auth\": {\"sapcloud-form\": {\"user\": \"$user\",\"pass\": \"$password\"}}}"
+rm -R shared/results/reports
 exec uiveri5 --v --useSeleniumJar true --config "{\"auth\": {\"sapcloud-form\": {\"user\": \"$user\",\"pass\": \"$password\"}}}"
